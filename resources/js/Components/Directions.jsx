5fa13b8f227906ef68d83b7f068abf5b14f4297d
Kirill Ulyanov <ulyanovwork@protonmail.com>
2024-12-05 13:01:03 +0500

    Fix Direct

--- resources/js/Components/Directions.jsx
+++ resources/js/Components/Directions.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DirectionsChlank from './DirectionsChlank';
 
-function Directions() {
+const Directions = () => {
+    const allDirections = [
+        { imgname: 1, title: "Медицинская наука", bgcolor: "bg-green-100", bgborder: "border-green-200"},
+        { imgname: 2, title: "Кадровые ресурсы", bgcolor: "bg-rose-100", bgborder: "border-rose-200"},
+        { imgname: 3, title: "Электронное здравоохранение", bgcolor: "bg-fuchsia-100", bgborder: "border-fuchsia-200"},
+        { imgname: 4, title: "Аккредитация", bgcolor: "bg-yellow-100", bgborder: "border-yellow-200"},
+        { imgname: 5, title: "Лекарственная политика", bgcolor: "bg-violet-100", bgborder: "border-violet-200"},
+        { imgname: 6, title: "Клинические протоколы", bgcolor: "bg-blue-100", bgborder: "border-blue-200"},
+        { imgname: 7, title: "Стратегические инициативы и междурародное сотрудничество", bgcolor: "bg-green-100", bgborder: "border-green-200"},
+        { imgname: 8, title: "Клинические протоколы", bgcolor: "bg-rose-100", bgborder: "border-rose-200"},
+        { imgname: 9, title: "Клинические протоколы", bgcolor: "bg-fuchsia-100", bgborder: "border-fuchsia-200"},
+        { imgname: 10, title: "Аккредитация", bgcolor: "bg-yellow-100", bgborder: "border-yellow-200"},
+        { imgname: 11, title: "Лекарственная политика", bgcolor: "bg-violet-100", bgborder: "border-violet-200"},
+        { imgname: 12, title: "Клинические протоколы", bgcolor: "bg-purple-200", bgborder: "border-purple-200"},
+        
+    ];
+
+    const [ showMore, setShowMore ] = useState(false);
+
   return (
     <section className="text-gray-600 body-font">
         <div className="container px-5 pb-24 mx-auto">
@@ -9,34 +27,33 @@ function Directions() {
                 <div className='flex'>
                     <h1 className="sm:text-2xl text-2xl font-semibold title-font text-gray-900 mb-2">Направления</h1>
                 </div>
-                <div className='flex'>
-                    <a
-                        className="cursor-pointer text-black inline-flex items-center border-gray-900 border-[1px] rounded-xl p-3">Все
-                        направления
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                            stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg>
-                    </a>
-                </div>
             </div>
             <div className="flex flex-wrap -m-4">
-                <DirectionsChlank imgname={1} title="Медицинская наука" bgcolor="bg-green-100"
-                    bgborder="border-green-200" />
-                <DirectionsChlank imgname={2} title="Кадровые ресурсы" bgcolor="bg-rose-100"
-                    bgborder="border-rose-200" />
-                <DirectionsChlank imgname={3} title="Электронное здравоохранение" bgcolor="bg-fuchsia-100"
-                    bgborder="border-fuchsia-200" />
-                <DirectionsChlank imgname={4} title="Аккредитация" bgcolor="bg-yellow-100"
-                    bgborder="border-yellow-200" />
-                <DirectionsChlank imgname={5} title="Лекарственная политика" bgcolor="bg-violet-100"
-                    bgborder="border-violet-200" />
-                <DirectionsChlank imgname={6} title="Клинические протоколы" bgcolor="bg-blue-100"
-                    bgborder="border-blue-200" />
+                {allDirections.slice(0, 6).map((direction, index) =>(
+                <DirectionsChlank key={index} imgname={direction.imgname} title={direction.title}
+                    bgcolor={direction.bgcolor} bgborder={direction.bgborder} />
+                ))}
+            </div>
+            {/* Скрытые направления */}
+            <div className={`flex flex-wrap -mx-4 mt-4 transition-all duration-500 delay-75 ease-in-out overflow-hidden ${ showMore
+                ? "max-h-[1000px]" : "max-h-0" }`}>
+                {allDirections.slice(6).map((direction, index) => (
+                <DirectionsChlank key={index} imgname={direction.imgname} title={direction.title}
+                    bgcolor={direction.bgcolor} bgborder={direction.bgborder} />
+                ))}
+            </div>
+
+            <div className="flex justify-center mt-4">
+                <button onClick={()=> setShowMore(!showMore)}
+                    className="cursor-pointer text-black inline-flex items-center border-gray-900 border-[1px]
+                    rounded-xl p-3 transition-all duration-150 ease-in"
+                    >
+                    {showMore ? "Скрыть направления" : "Все направления"}
+                </button>
             </div>
         </div>
     </section>
-     )
+  );
 }
 
 export default Directions
