59f5264eb065876fa3284b5332d8862c91d7c953
Kirill Ulyanov <ulyanovwork@protonmail.com>
2024-12-10 20:35:30 +0500

    Fix Hero

--- resources/js/Components/Hero.jsx
+++ resources/js/Components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero({img, h1}) {
                 <img className="absolute inset-0 w-full h-full object-cover" alt="hero"
                     src={`./img/HeroImg/${img}.png`} />
                 <div
-                    className="relative lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    className="relative h-full lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font text-4xl my-auto font-semibold text-gray-900">
                         {h1}
                     </h1>
