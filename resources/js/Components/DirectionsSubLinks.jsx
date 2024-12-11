e055c3af42eabe6b6447137a6ac0203550175da8
Kirill Ulyanov <ulyanovwork@protonmail.com>
2024-12-12 02:24:29 +0500

    Fix subMenu

---
+++ resources/js/Components/DirectionsSubLinks.jsx
@@ -0,0 +1,14 @@
+import { Link } from '@inertiajs/react'
+import React from 'react'
+
+function DirectionsSubLinks({ title, url }) {
+  return (
+    <li>
+        <Link href={route(url)} className="block px-6 py-4 mx-4 text-sm text-gray-700 hover:bg-gray-100 rounded-xl transition">
+            {title}
+        </Link>
+    </li>
+  )
+}
+
+export default DirectionsSubLinks
--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -1,10 +1,27 @@
 import { Link } from '@inertiajs/react';
 import React, {useState, useEffect} from 'react';
+import DirectionsSubLinks from './DirectionsSubLinks';
 
 export default function Header() {
     const [isScrolled, setIsScrolled] = useState(false);
     const [menuOpen, setMenuOpen] = useState(false);
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const allDirectionsSubLinks = [
+        { title: "Медицинское образование", url: "medical.education"},
+        { title: "Кадровые ресурсы", url: "human.resources"},
+        { title: "Электронное здравоохранение", url: "electronic.health"},
+        { title: "Аккредитация", url: "medical.accreditation"},
+        { title: "Оценка технологий здравоохранения", url: "health.rate"},
+        { title: "Клинические протоколы", url: "clinical.protocols"},
+        { title: "Междурародное сотрудничество", url: "medical.education"},
+        { title: "Рейтинг медицинских организаций", url: "medical.education"},
+        { title: "Медицинская наука", url: "medical.education"},
+        { title: "Лекарственная политика", url: "medical.education"},
+        { title: "Первичная медико-санитарная помощь", url: "medical.education"},
+        { title: "Национальные счета здравоохранения", url: "medical.education"},
+        
+    ];
+
 
     useEffect(() => {
         const handleScroll = () => {
@@ -18,90 +35,73 @@ export default function Header() {
         };
     }, []);
   return (
-    <header className={`fixed top-0 text-gray-600 body-font z-50 w-full ease-in duration-150 ${ isScrolled
+    <header className={`fixed top-0 text-gray-600 font-medium body-font z-50 w-full ease-in duration-150 ${ isScrolled
         ? "bg-white shadow-md" : "bg-transparent" }`}>
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <Link href={route('home')} className="hidden lg:flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                <span className="text-xs uppercase">национальный научный центр развития <br />здравоохранения им.
-                    салидат
-                    каирбековой</span>
+            <Link href={route('home')}
+                className="hidden lg:flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span className="text-xs uppercase">национальный научный центр развития <br />здравоохранения им.
+                салидат
+                каирбековой</span>
             </Link>
             <nav className="hidden md:ml-auto md:mr-auto lg:flex flex-wrap items-center text-base justify-center">
-            <div className="relative group">
-                {/* Кнопка */}
-                <button className="mr-5 hover:text-gray-900 focus:outline-none flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                        className="mr-2 h-4 w-4 transition-transform group-hover:rotate-180" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-                    </svg>
-                    Направления
-                </button>
+                <div className="relative group mr-8">
+                    {/* Кнопка */}
+                    <button className="group-hover:text-gray-900 focus:outline-none flex items-center cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            className="mr-2 h-4 w-4 transition-transform group-hover:rotate-180" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                        </svg>
+                        Направления
+                    </button>
 
-                {/* Меню */}
-                <ul className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-md py-2 z-50 transform
-      transition-all duration-150 ease-in-out opacity-0 scale-95 invisible group-hover:opacity-100 group-hover:scale-100 group-hover:visible"
-                    style={{ transformOrigin: "top center" }}>
-                    <li>
-                        <a href="#" className="block px-8 py-4 text-sm text-gray-700 hover:bg-gray-100 transition">
-                            Медицинское образование
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="block px-8 py-4 text-sm text-gray-700 hover:bg-gray-100 transition">
-                            Кадровые ресурсы
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="block px-8 py-4 text-sm text-gray-700 hover:bg-gray-100 transition">
-                            Электронное здравоохранение
+                    {/* Контейнер для двойного меню */}
+                    <div className="fixed left-1/2 -translate-x-1/2 transform mt-8 bg-white border-[1px] border-gray-300 shadow-lg rounded-xl py-2 z-50 
+    transition-all duration-150 ease-in-out opacity-0 scale-95 invisible group-hover:opacity-100 group-hover:scale-100 group-hover:visible flex"
+                        style={{ transformOrigin: "top center" }}>
+                        {/* Первое меню */}
+                        <ul className="w-96 py-2 border-r">
+                            {allDirectionsSubLinks.slice(0, 6).map((directionsublink, index) =>(
+                                <DirectionsSubLinks key={index} title={directionsublink.title} url={directionsublink.url} />
 
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="block px-8 py-4 text-sm text-gray-700 hover:bg-gray-100 transition">
-                            Аккредитация медицинских организаций
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="block px-8 py-4 text-sm text-gray-700 hover:bg-gray-100 transition">
-                            Оценка технологий здравоохранения
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="block px-8 py-4 text-sm text-gray-700 hover:bg-gray-100 transition">
-                            Клинические протоколы
-                        </a>
-                    </li>
-                </ul>
+                            ))}
+                        </ul>
 
-                {/* Увеличенная зона интерактивности */}
-                <div className="absolute inset-0 -top-4 -left-4 -right-4 -bottom-4"></div>
-            </div>
+                        {/* Второе меню */}
+                        <ul className="w-96 py-2">
+                            {allDirectionsSubLinks.slice(6).map((directionsublink, index) =>(
+                                <DirectionsSubLinks key={index} title={directionsublink.title} url={directionsublink.url} />
+                            ))}
 
+                        </ul>
+                    </div>
 
-    
-                <a className="mr-5 hover:text-gray-900">Услуги</a>
-                <a className="mr-5 hover:text-gray-900">О центре</a>
-                <a className="mr-5 hover:text-gray-900">Филиалы</a>
-                <a className="mr-5 hover:text-gray-900">Новости</a>
+                    {/* Увеличенная зона интерактивности */}
+                    <div className="absolute inset-0 -top-8 -bottom-8 cursor-pointer"></div>
+                </div>
+
+                <a className="mr-8 hover:text-gray-900">Услуги</a>
+                <a className="mr-8 hover:text-gray-900">О центре</a>
+                <a className="mr-8 hover:text-gray-900">Филиалы</a>
+                <a className="mr-8 hover:text-gray-900">Новости</a>
             </nav>
-            <div className='hidden lg:flex flex-wrap items-end text-base justify-end'>
+            <div className='hidden lg:flex flex-wrap items-end justify-end'>
                 <button
-                    className="mx-1 inline-flex items-center hover:border-b-2 border-black py-1 px-3 focus:outline-none text-xs mt-4 md:mt-0 transition-all ease-in duration-150">EN
+                    className="mx-1 inline-flex items-center hover:border-b-2 border-black py-1 px-3 focus:outline-none text-sm mt-4 md:mt-0 transition-all ease-in duration-150">EN
                 </button>
                 <button
-                    className="mx-1 inline-flex items-center border-b-2 border-black py-1 px-3 focus:outline-none text-xs mt-4 md:mt-0 transition-all ease-in duration-150">RU
+                    className="mx-1 inline-flex items-center border-b-2 border-black py-1 px-3 focus:outline-none text-sm text-gray-900 mt-4 md:mt-0 transition-all ease-in duration-150">RU
                 </button>
                 <button
-                    className="mx-1 inline-flex items-center hover:border-b-2 border-black py-1 px-3 focus:outline-none text-xs mt-4 md:mt-0 transition-all ease-in duration-150">KZ
+                    className="mx-1 inline-flex items-center hover:border-b-2 border-black py-1 px-3 focus:outline-none text-sm mt-4 md:mt-0 transition-all ease-in duration-150">KZ
                 </button>
             </div>
 
             {/* Бургер-меню (мобильная версия) */}
             <div className="lg:hidden flex justify-between w-full">
                 <Link href={route('home')} className="flex font-medium items-start text-gray-900 mb-4 md:mb-0">
-                    <span className="text-2xl uppercase">NNCRZ</span>
+                <span className="text-2xl uppercase">NNCRZ</span>
                 </Link>
                 <button onClick={()=> setMenuOpen(!menuOpen)}
                     className="text-gray-900 focus:outline-none content-center mb-4"
